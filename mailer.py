26a977c3dd532ee8d1571fdc1ff73a8f692fe2a1
KimPeek <kimpeek@users.noreply.github.com>
2017-04-01 21:30:28 -0500

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,19 +1,39 @@
+from time import sleep, time
 import logging
 import sqlite3
-import sys
 import praw
 
-TARGET_SUB = 'redditrequest'
-REDDIT = praw.Reddit('AUTHENTICATION')
-CONN = sqlite3.connect('approvals.db')
-CUR = CONN.cursor()
-LOGGER = logging.getLogger('Mailer')
+reddit = praw.Reddit('AUTHENTICATION')
+conn = sqlite3.connect('approvals.db')
+logger = logging.getLogger('Mailer')
 logging.basicConfig(
     filename='log.txt',
     filemode='a',
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(name)s %(message)s')
-MESSAGE = """
+
+
+def main():
+    logger.info('Start')
+    start = time()
+    sleep(5)
+    while True:
+        try:
+            for submission in reddit.subreddit('redditrequest').stream.submissions():
+                if submission.created_utc < start:
+                    continue
+                handle(submission.author.name.lower())
+        except Exception as error:
+            logger.exception(error)
+            sleep(30)
+
+
+def handle(author):
+    if author in {n[0] for n in conn.execute('SELECT user FROM blacklist')} and author != 'kimpeek':
+        return None
+    else:
+
+        message = """
 The current backlog for /r/redditrequest moderators is approximately {} days.
 
 Please visit /r/redditapprovals for more information.
@@ -21,49 +41,17 @@ Please visit /r/redditapprovals for more information.
 ---
 
 ^^This ^^is ^^a ^^bot. ^^You ^^will ^^never ^^be ^^messaged ^^about ^^this ^^again.
-""".format([s for s in CONN.execute('SELECT * FROM stats')][0][0])
+        """.format([s for s in conn.execute('SELECT * FROM stats')][0][0])
 
-
-def main():
-    generator = REDDIT.subreddit(TARGET_SUB).stream.submissions()
-    try:
-        for submission in generator:
-            handle(submission.author.name.lower())
-    except Exception as error:
-        LOGGER.exception(error)
-        sys.exit()
-
-
-def handle(author):
-    if author in {n[0] for n in CONN.execute('SELECT user FROM blacklist')}:
-        return None
-    else:
         try:
-            REDDIT.redditor(author).message('Estimated Wait Time', MESSAGE)
+            reddit.redditor(author).message('Estimated Wait Time', message)
         except Exception as error:
-            LOGGER.exception(error)
-            sys.exit()
+            logger.exception(error)
         else:
-            CONN.execute('INSERT INTO blacklist VALUES (?)', (author,))
-            CONN.commit()
-
-
-def build_db():
-    CONN.execute('CREATE TABLE IF NOT EXISTS blacklist (user)')
-    try:
-        mod_gen = REDDIT.subreddit(TARGET_SUB).moderator()
-    except Exception as error:
-        LOGGER.exception(error)
-        sys.exit()
-    else:
-        existing = {n[0] for n in CONN.execute('SELECT user FROM blacklist')}
-        for mod in mod_gen:
-            if mod in existing:
-                continue
-            CONN.execute('INSERT INTO blacklist VALUES (?)', (mod.name.lower(),))
-        CONN.commit()
+            conn.execute('INSERT INTO blacklist VALUES (?)', (author,))
+            conn.commit()
+            return
 
 
 if __name__ == '__main__':
-    build_db()
     main()
